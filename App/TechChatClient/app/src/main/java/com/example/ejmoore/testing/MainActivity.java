6546765ce83fb54d9a633f8dc0644b80f003b496
JamesGiannunzio <jagiannu@mtu.edu>
2017-03-29 11:34:14 -0400

    use "username" and "password" to enter app

--- App/TechChatClient/app/src/main/java/com/example/ejmoore/testing/MainActivity.java
+++ App/TechChatClient/app/src/main/java/com/example/ejmoore/testing/MainActivity.java
@@ -5,14 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    EditText username = null;
+    EditText password = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.editUsername);
+        password = (EditText)findViewById(R.id.editPassword);
 
         Button loginOrRegister = (Button) findViewById(R.id.loginOrRegister);
         loginOrRegister.setOnClickListener(this);
@@ -24,7 +31,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         switch (v.getId()) { //allows the switching of jobs based on id of button
             case R.id.loginOrRegister:
-                startActivity(new Intent(MainActivity.this, HomePage.class));
+                if(username.getText().toString().equals("username") &&
+                        password.getText().toString().equals("password")) {
+                    startActivity(new Intent(MainActivity.this, HomePage.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong username or password. try 'username' and 'password'", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
