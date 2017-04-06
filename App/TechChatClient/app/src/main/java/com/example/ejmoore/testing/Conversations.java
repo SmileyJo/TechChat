141f7768cdf4b5d9b806acfd80bd7a87e9a83aa1
JamesGiannunzio <jagiannu@mtu.edu>
2017-04-06 16:39:05 -0400

    added conversations page

---
+++ App/TechChatClient/app/src/main/java/com/example/ejmoore/testing/Conversations.java
@@ -0,0 +1,21 @@
+package com.example.ejmoore.testing;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.ListView;
+
+public class Conversations extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversations);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
